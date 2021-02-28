3cab0500455e61aa368de10bc7e54eeb78c3bdfc
Juan J. Barriga García <32481144+PowerOfMind@users.noreply.github.com>
2021-02-28 16:26:39 +0100

    poperties de extension y sobrecarga de operadores

--- app/src/main/java/com/jjbarriga/myplayer/test2.kt
+++ app/src/main/java/com/jjbarriga/myplayer/test2.kt
@@ -1,5 +1,8 @@
 package com.jjbarriga.myplayer
 
+import android.view.View
+import android.view.ViewGroup
+
 object MyObject {
     val x = 20
     val y = "Hello"
@@ -11,3 +14,14 @@ fun test2() {
     MyObject.x
     MyObject.y
 }
+
+fun testViewGroup(viewGroup: ViewGroup){
+    viewGroup.getChildAt(0)    //ORIGINAL
+    viewGroup.size          //FUNCIONES DE EXTENSION
+    viewGroup[0]
+}
+
+val ViewGroup.size: Int
+    get() = childCount
+
+operator fun ViewGroup.get(index:Int): View = getChildAt(index)
